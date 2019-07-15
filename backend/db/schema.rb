78d12a6a0e995bcb76a22c6f011d398e670fe9c0
melvynMF <melvyn.mf@gmail.com>
2019-07-15 23:40:19 +0200

    ajout article api

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_144317) do
+ActiveRecord::Schema.define(version: 2019_07_13_192907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.string "manufacturer"
+    t.string "design"
+    t.string "style"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "motto"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -39,5 +64,6 @@ ActiveRecord::Schema.define(version: 2019_06_26_144317) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "items", "todos"
 end
